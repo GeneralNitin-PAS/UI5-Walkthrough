19bed7961f57483e62902129ccafe6ab2a96bc7e
Nitin Kumar <nitin.kumar15@sap.com>
2022-01-21 21:27:52 +0530

    step 4: xml views and customising namespaces

--- webapp/index.js
+++ webapp/index.js
@@ -1,10 +1,11 @@
 sap.ui.define([
-    "sap/m/Text"
-], function (Text) {
+    "sap/ui/core/mvc/XMLView"
+], function (XMLView) {
     "use strict";
 
-    new Text({
-        text: "Hello World"
-    }).placeAt("content");
-    // alert("UI5 is ready");
+    XMLView.create({
+        viewName: "myNamespace.view.App"
+    }).then(function(oView){
+        oView.placeAt("content");
+    });
 });
